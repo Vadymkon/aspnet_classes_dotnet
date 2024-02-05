c3bebedb072f15d87880fd553afd57ec16a4f944
Vadymkon <vadykakonon@gmail.com>
2024-02-05 22:29:35 +0200

    First commit

---
+++ ClassLibrary/ClassLibrary/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Library_1
+{
+    public class KI3_Class_1
+    {
+        public int F1(int x,int y)
+        {
+            return 3*x+y;
+        }
+    }
+}
+namespace Library_2
+{
+    public static class KI3_Class_2
+    {
+        public static int F2(int x, int y)
+        {
+            return x+2*y;
+        }
+    }
+}
+
+namespace Library_3
+{
+    public class KI3_Class_3 : Library_1.KI3_Class_1
+    {
+        public int F3(int x, int y)
+        {
+            return 4 * this.F1(x, y)-Library_2.KI3_Class_2.F2(x,y);
+        }
+    }
+}
+
+namespace Library_4
+{
+    public static class KI3_Class_4
+    {
+        public static int F4(int x, int y)
+        {
+            return 5*Library_2.KI3_Class_2.F2(x,y)-3;
+        }
+    }
+}
---
+++ WebPageForLib/WebPageForLib/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Library_1;
+using Library_2;
+using Library_3;
+using Library_4;
+
+namespace WebPageForLib
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Library_3.KI3_Class_3 a = new KI3_Class_3();
+            int x = Convert.ToInt32(TextBox1.Text);
+            int y = Convert.ToInt32(TextBox2.Text);
+            Label1.Text = (a.F3(x,y) + 3*Library_4.KI3_Class_4.F4(x,y)).ToString();
+        }
+    }
+}
